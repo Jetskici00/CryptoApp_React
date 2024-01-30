04db529c3c3126e7ccb265fe1ea7842d5054eeeb
jetski.ci <gidrokisik@gmail.com>
2024-01-30 12:09:46 +0500

    Header, modal

--- src/context/crypto-context.jsx
+++ src/context/crypto-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 import { fakeFetchCrypto, fetchAssets } from "../api";
 import { percentDifference } from "../utils";
 
@@ -36,7 +36,7 @@ export function CryptoContextProvider({ children }) {
     }
     preload();
   }, []);
-  
+
   return (
     <CryptoContext.Provider value={{ loading, crypto, assets }}>
       {children}
@@ -45,3 +45,7 @@ export function CryptoContextProvider({ children }) {
 }
 
 export default CryptoContext;
+
+export function useCrypto() {
+  return useContext(CryptoContext)
+}
